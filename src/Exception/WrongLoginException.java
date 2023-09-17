13a6377ec6c8025230a16eef7c9b88f41aa892fe
chibi <chibis_y@mail.ru>
2023-09-17 21:53:43 +0300

    Работа с исключениями

---
+++ src/Exception/WrongLoginException.java
@@ -0,0 +1,12 @@
+package Exception;
+
+public class WrongLoginException extends Exception{
+    public WrongLoginException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
